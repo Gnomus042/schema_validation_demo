6f58502d9ddbea6317c7c7342930de34f05e0000
Anastasiia Byvsheva <gnomus042@gmail.com>
2020-09-29 17:04:52 +0300

    Failure message: Wrong replaced with unexpected

--- static/js/libs/schemarama.bundle.js
+++ static/js/libs/schemarama.bundle.js
@@ -1,19 +1,3 @@
-/*
- * Copyright 2020 Anastasiia Byvsheva & Dan Brickley
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *      https://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
-
 var schemarama =
 /******/ (function(modules) { // webpackBootstrap
 /******/ 	// The module cache
@@ -24850,7 +24834,7 @@ class ValidationReport {
         };
         switch (jsonReport.type) {
             case 'TypeMismatch':
-                failure.message = `Value provided for property ${failure.property} has a wrong type`;
+                failure.message = `Value provided for property ${failure.property} has an unexpected type`;
                 this.simplify(jsonReport.errors, undefined, undefined);
                 break;
             case 'MissingProperty':
